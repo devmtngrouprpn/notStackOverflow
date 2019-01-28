964b8cd714109908a466a74a4fdba8be3c024ef9
Payton Alexander Whipple <payton@Paytons-MacBook-Pro.local>
2019-01-28 11:35:21 -0700

    make skeleton server

---
+++ server/index.js
@@ -0,0 +1,39 @@
+require('dotenv').config()
+const { DB_CONNECTION_STRING, SERVER_PORT, SESSION_SECRET, DEV } = process.env;
+
+const express = require('express');
+const session = require('express-session');
+const massive = require('massive');
+
+const app = express();
+// *** TOPLEVEL MIDDLEWARE *** //
+
+app.use(express.json());
+app.use(session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+}));
+massive(DB_CONNECTION_STRING)
+    .then(db => {
+        app.set('db', db);
+    });
+app.use((req, res, next) => {
+    if (DEV = 'true') {
+        if (!req.session.user) {
+            req.session.user = {};
+            next();
+        } else {
+            next();
+        }
+    } else {
+        next();
+    }
+});
+// *** ENDPOINTS *** //
+
+Happy_hacking!
+
+// *** IM LISTENING! *** //
+
+app.listen(SERVER_PORT, () => console.log(`Listening on PORT: SERVER_PORT`));
