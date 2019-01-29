8cfeb5b8c407070118eb1b3023556d1fad55b01d
Ryan Hutchison <ryanzhutch@gmail.com>
2019-01-29 12:01:45 -0700

    added db folder

---
+++ db/dummy_data.sql
@@ -0,0 +1,26 @@
+insert into users (auth_id, username, reputation, user_created, user_first_last, personal_site, location, facebook, git_hub, twitter, occupation, bio, tags_watching, profile_views, last_logout)
+values('user1','HGIRyan',1,NOW(),'Ryan Hutchison','','','','','','','','{javascript, sql, react}',1,NOW())
+
+insert into users (auth_id, username, reputation, user_created, user_first_last, personal_site, location, facebook, git_hub, twitter, occupation, bio, tags_watching, profile_views, last_logout)
+values('user2','PWhip',1,NOW(),'Payton Whipple ','','','','','','','','{javascript, sql, react}',1,NOW());
+
+
+select * from users
+
+insert into question (user_id, tags, question_views, question_creation_timestamp, question_content, question_deleted, modified)
+values ('user1', '{javascript, react}', 1, NOW(), 'Im using Rails 4.1 and Postgresql (with PG gem) as my database. I have a very stand many to many association from companies to provinces with a join table called regions. Now obviously the regions table has no primary key cause I used { :id => false }. But when I try to use depending destroy or just simply calling destroy on the region object it self I get this error:', false, false)
+
+
+insert into question (user_id, tags, question_views, question_creation_timestamp, question_content, question_deleted, modified)
+values ('user1', '{javascript, react}', 1, NOW(), 'This is a question about how dumb some coding things are', false, false)
+
+
+insert into question (user_id, tags, question_views, question_creation_timestamp, question_content, question_deleted, modified)
+values ('user2', '{javascript, react}', 1, NOW(), 'I know the problem is caused due to the lack of a primary key for the regions table. And oddly if I add the primary key back to the table destroy works fine and no error. However, if I remove the primary key from the table the error comes back. I know this has something to do with the postgres but Ive no idea how to solve this without having to add a primary key column to my regions table.
+Here is the actual query
+', false, false)
+
+
+insert into question (user_id, tags, question_views, question_creation_timestamp, question_content, question_deleted, modified)
+values ('user2', '{javascript, vue}', 1, NOW(), 'I have a group chat message build using Vue.js. I am currently fetching the messages which returns an array like this:', false, false)
+
---
+++ db/seed_file.sql
@@ -0,0 +1,257 @@
+-- initialize DB Start --
+CREATE TABLE "user" (
+	"auth_id" VARCHAR(255) NOT NULL,
+	"username" TEXT NOT NULL,
+	"reputation" integer NOT NULL,
+	"url" TEXT NOT NULL,
+	"user_created" TIMESTAMP NOT NULL,
+	"user_first_last" TEXT,
+	"personal_site" varchar(300) NOT NULL,
+	"location" varchar(300) NOT NULL,
+	"facebook" varchar(300) NOT NULL,
+	"git_hub" varchar(300) NOT NULL,
+	"twitter" varchar(300) NOT NULL,
+	"occupation" varchar(200) NOT NULL,
+	"bio" varchar(300) NOT NULL,
+	"tags_watching" text[] NOT NULL,
+	"profile_views" integer NOT NULL,
+	"last_logout" TIMESTAMP NOT NULL,
+	CONSTRAINT user_pk PRIMARY KEY ("auth_id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "question" (
+	"question_id" serial NOT NULL,
+	"user_id" integer NOT NULL,
+	"tags" text[] NOT NULL,
+	"question_views" integer NOT NULL,
+	"question_last_edit" TIMESTAMP NOT NULL,
+	"question_creation_timestamp" TIMESTAMP NOT NULL,
+	"question_content" TEXT NOT NULL,
+	"question_deleted" BOOLEAN NOT NULL,
+	"modified" BOOLEAN NOT NULL,
+	"modified_time" TIMESTAMP NOT NULL,
+	CONSTRAINT question_pk PRIMARY KEY ("question_id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "tag" (
+	"name" varchar(30) NOT NULL,
+	"tag_views" integer NOT NULL,
+	"description" TEXT NOT NULL,
+	CONSTRAINT tag_pk PRIMARY KEY ("name")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "bounty" (
+	"bounty_id" serial NOT NULL,
+	"question_id" integer NOT NULL,
+	"user_id" integer NOT NULL,
+	"bounty_winner" TEXT,
+	"bounty_value" integer NOT NULL,
+	"bounty_creation_timestamp" TIMESTAMP NOT NULL,
+	"bounty_active" BOOLEAN NOT NULL,
+	CONSTRAINT bounty_pk PRIMARY KEY ("bounty_id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "answer" (
+	"answer_id" serial NOT NULL,
+	"user_id" TEXT NOT NULL,
+	"question_id" integer NOT NULL,
+	"answer_content" TEXT NOT NULL,
+	"answer_creation_timestamp" TIMESTAMP NOT NULL,
+	"answer_last_edit" TIMESTAMP NOT NULL,
+	"answer_views" integer NOT NULL,
+	"answer_accepted" BOOLEAN NOT NULL,
+	"answer_deleted" BOOLEAN NOT NULL,
+	"modified" BOOLEAN NOT NULL,
+	"modified_time" TIMESTAMP NOT NULL,
+	CONSTRAINT answer_pk PRIMARY KEY ("answer_id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "comments" (
+	"comment_id" serial NOT NULL,
+	"source_id" integer NOT NULL,
+    "source_type" text not null,
+	"content" TEXT NOT NULL,
+	CONSTRAINT comments_pk PRIMARY KEY ("comment_id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "question_tag" (
+	"tag_name" TEXT NOT NULL,
+	"question_id" integer NOT NULL
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "badge" (
+	"badge_id" serial NOT NULL,
+	"name" serial NOT NULL,
+	"class" serial NOT NULL,
+	"description" serial NOT NULL,
+	CONSTRAINT badge_pk PRIMARY KEY ("badge_id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "badge_user" (
+	"badge_user_id" serial NOT NULL,
+	"user_id" TEXT NOT NULL,
+	"badge_id" integer NOT NULL,
+	"completion" integer[] NOT NULL,
+	"awarded" BOOLEAN NOT NULL,
+	"date_awarded" TIMESTAMP,
+	CONSTRAINT badge_user_pk PRIMARY KEY ("badge_user_id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "reputation" (
+	"reputation_id" serial NOT NULL,
+	"reputation_time" TIMESTAMP NOT NULL,
+	"user_id" TEXT NOT NULL,
+	"amount" integer NOT NULL,
+	"action_type" TEXT NOT NULL,
+	"question_source_id" integer NOT NULL,
+	CONSTRAINT reputation_pk PRIMARY KEY ("reputation_id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "vote" (
+	"vote_id" serial NOT NULL,
+	"user_id" TEXT NOT NULL,
+	"up_or_down" BOOLEAN NOT NULL,
+	"source_id" integer NOT NULL,
+	"source_type" TEXT NOT NULL,
+	CONSTRAINT vote_pk PRIMARY KEY ("vote_id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "edit" (
+	"edit_id" serial NOT NULL,
+	"user_id" serial NOT NULL,
+	"edit_date" TIMESTAMP NOT NULL,
+	"edit_summary" varchar(500) NOT NULL,
+	"source_id" integer NOT NULL,
+	"source_type" TEXT NOT NULL,
+	CONSTRAINT edit_pk PRIMARY KEY ("edit_id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+
+ALTER TABLE "question" ADD CONSTRAINT "question_fk0" FOREIGN KEY ("user_id") REFERENCES "user"("auth_id");
+
+
+ALTER TABLE "bounty" ADD CONSTRAINT "bounty_fk0" FOREIGN KEY ("question_id") REFERENCES "question"("question_id");
+ALTER TABLE "bounty" ADD CONSTRAINT "bounty_fk1" FOREIGN KEY ("user_id") REFERENCES "user"("auth_id");
+ALTER TABLE "bounty" ADD CONSTRAINT "bounty_fk2" FOREIGN KEY ("bounty_winner") REFERENCES "user"("auth_id");
+
+ALTER TABLE "answer" ADD CONSTRAINT "answer_fk0" FOREIGN KEY ("user_id") REFERENCES "user"("auth_id");
+ALTER TABLE "answer" ADD CONSTRAINT "answer_fk1" FOREIGN KEY ("question_id") REFERENCES "question"("question_id");
+
+
+ALTER TABLE "question_tag" ADD CONSTRAINT "question_tag_fk0" FOREIGN KEY ("tag_name") REFERENCES "tag"("name");
+ALTER TABLE "question_tag" ADD CONSTRAINT "question_tag_fk1" FOREIGN KEY ("question_id") REFERENCES "question"("question_id");
+
+
+ALTER TABLE "badge_user" ADD CONSTRAINT "badge_user_fk0" FOREIGN KEY ("user_id") REFERENCES "user"("auth_id");
+ALTER TABLE "badge_user" ADD CONSTRAINT "badge_user_fk1" FOREIGN KEY ("badge_id") REFERENCES "badge"("badge_id");
+
+ALTER TABLE "reputation" ADD CONSTRAINT "reputation_fk0" FOREIGN KEY ("user_id") REFERENCES "user"("auth_id");
+
+ALTER TABLE "vote" ADD CONSTRAINT "vote_fk0" FOREIGN KEY ("user_id") REFERENCES "user"("auth_id");
+
+ALTER TABLE "edit" ADD CONSTRAINT "edit_fk0" FOREIGN KEY ("user_id") REFERENCES "user"("auth_id");
+
+
+-- initialize DB End --
+-- Drop DB Start --
+ALTER TABLE "question" DROP CONSTRAINT IF EXISTS "question_fk0";
+
+ALTER TABLE "bounty" DROP CONSTRAINT IF EXISTS "bounty_fk0";
+
+ALTER TABLE "bounty" DROP CONSTRAINT IF EXISTS "bounty_fk1";
+
+ALTER TABLE "bounty" DROP CONSTRAINT IF EXISTS "bounty_fk2";
+
+ALTER TABLE "answer" DROP CONSTRAINT IF EXISTS "answer_fk0";
+
+ALTER TABLE "answer" DROP CONSTRAINT IF EXISTS "answer_fk1";
+
+ALTER TABLE "question_tag" DROP CONSTRAINT IF EXISTS "question_tag_fk0";
+
+ALTER TABLE "question_tag" DROP CONSTRAINT IF EXISTS "question_tag_fk1";
+
+ALTER TABLE "badge_user" DROP CONSTRAINT IF EXISTS "badge_user_fk0";
+
+ALTER TABLE "badge_user" DROP CONSTRAINT IF EXISTS "badge_user_fk1";
+
+ALTER TABLE "reputation" DROP CONSTRAINT IF EXISTS "reputation_fk0";
+
+ALTER TABLE "vote" DROP CONSTRAINT IF EXISTS "vote_fk0";
+
+ALTER TABLE "edit" DROP CONSTRAINT IF EXISTS "edit_fk0";
+
+DROP TABLE IF EXISTS "user";
+
+DROP TABLE IF EXISTS "question";
+
+DROP TABLE IF EXISTS "tag";
+
+DROP TABLE IF EXISTS "bounty";
+
+DROP TABLE IF EXISTS "answer";
+
+DROP TABLE IF EXISTS "comments";
+
+DROP TABLE IF EXISTS "question_tag";
+
+DROP TABLE IF EXISTS "badge";
+
+DROP TABLE IF EXISTS "badge_user";
+
+DROP TABLE IF EXISTS "reputation";
+
+DROP TABLE IF EXISTS "vote";
+
+DROP TABLE IF EXISTS "edit";
+
+-- Drop DB END --
+-- DB Queries Start --
+-- DB Queries END --
