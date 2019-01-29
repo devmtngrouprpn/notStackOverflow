115e48f6b347bd2a99569105a5bf2a79a3ac9475
Ryan Hutchison <ryanzhutch@gmail.com>
2019-01-29 16:33:52 -0700

    save

--- server/index.js
+++ server/index.js
@@ -1,43 +1,55 @@
-require('dotenv').config()
+require("dotenv").config();
 const { DB_CONNECTION_STRING, SERVER_PORT, SESSION_SECRET, DEV } = process.env;
 
-const express = require('express');
-const session = require('express-session');
-const massive = require('massive');
-const authorization = require('./controllers/authorization')
-const questions = require('./controllers/questions')
-
+const express = require("express");
+const session = require("express-session");
+const massive = require("massive");
+const authorization = require("./controllers/authorization");
+const questions = require("./controllers/questions");
 
 const app = express();
 // *** TOPLEVEL MIDDLEWARE *** //
 
 app.use(express.json());
-app.use(session({
+app.use(
+  session({
     secret: SESSION_SECRET,
     resave: false,
-    saveUninitialized: false,
-}));
-massive(DB_CONNECTION_STRING)
-    .then(db => {
-        app.set('db', db);
-    });
-app.use((req, res, next) => {
-    if (DEV = 'true') {
-        if (!req.session.user) {
-            req.session.user = {};
-            next();
-        } else {
-            next();
-        }
-    } else {
-        next();
-    }
+    saveUninitialized: false
+  })
+);
+massive(DB_CONNECTION_STRING).then(db => {
+  app.set("db", db);
 });
+// app.use((req, res, next) => {
+//     if (DEV = 'true') {
+//         if (!req.session.user) {
+//             req.session.user = {};
+//             next();
+//         } else {
+//             next();
+//         }
+//     } else {
+//         next();
+//     }
+// });
 // *** ENDPOINTS *** //
 
-app.get('/auth/callback', authorization.authCallback)
-app.get('/api/user-data', authorization.checkUser)
-app.get(`/api/home`, questions.homeStart)
+app.get("/auth/callback", authorization.authCallback);
+app.get("/api/user-data", authorization.checkUser);
+
+// HOME START
+app.get("/api/home/:auth_id", questions.homeStart);
+app.get("/api/questions/interesting", questions.questionsInteresting);
+// app.get("/api/questions/featured", questions.questionsFeatured);
+// app.get("/api/questions/hot", questions.questionsHot);
+// app.get("/api/questions/week", questions.questionsWeek);
+// app.get("/api/questions/month", questions.questionsMonth);
+// HOME END
+// TAGS START
+// TAGS END
+// USERS START
+// USERS END
 
 // *** IM LISTENING! *** //
 
