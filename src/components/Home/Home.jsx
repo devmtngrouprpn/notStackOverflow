89df00bb20f4b2ef75492f0d5902b0f75f9f5814
Payton Alexander Whipple <payton@Paytons-MacBook-Pro.local>
2019-01-30 14:24:03 -0700

    save

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,13 +1,12 @@
-import React from 'react';
-import Layout from '../Layout/Layout1.jsx';
-import HQCard from './../Questions/HQCard'
+import React from "react";
+import Layout from "../Layout/Layout1.jsx";
 
 export default function Home(props) {
-    return (
-        <>
-            <Layout>
-                <h1>Home Page</h1>
-            </Layout>
-        </>
-    )
-};
+  return (
+    <>
+      <Layout>
+        <h1>Home Page</h1>
+      </Layout>
+    </>
+  );
+}
--- src/components/Layout/Header/SearchBar.jsx
+++ src/components/Layout/Header/SearchBar.jsx
@@ -1,39 +1,38 @@
-import React, { Component } from 'react';
-import styled from 'styled-components';
-// import { headerGrays } from '../../../utilites/index.js';
-
+import React, { Component } from "react";
+import styled from "styled-components";
+import { blueButton, boxShadowBlue } from "../../../utilites/index.js";
 
 class SearchBar extends Component {
+  state = {};
 
-    state = {
-
-    }
-
-
-    render() {
-        return (
-            <>
-                <SearchBox placeholder='Search...' />
-            </>
-        )
-    }
+  render() {
+    return (
+      <>
+        <SearchBox placeholder="Search..." />
+      </>
+    );
+  }
 }
 
 export default SearchBar;
 
 const SearchBox = styled.input`
-border-radius:3px;
-border-color: #BBC0C4;
-border: 1px solid lightgray;
-background-color: #FFF;
-box-shadow: none;
-color: #3b4045;
-max-width: 750px;
-width: 750px;
-box-sizing: border-box;
-margin-left:12px;
- padding: 8px 9px 8px 9px;
- ::placeholder{
-     color:lightgray;
- }
+  border-radius: 3px;
+  border-color: #bbc0c4;
+  border: 1px solid lightgray;
+  background-color: #fff;
+  box-shadow: none;
+  color: #3b4045;
+  max-width: 750px;
+  width: 750px;
+  box-sizing: border-box;
+  margin-left: 12px;
+  padding: 8px 9px 8px 9px;
+  :focus {
+    outline: none;
+    border: 1px solid ${boxShadowBlue};
+  }
+  ::placeholder {
+    color: lightgray;
+  }
 `;
