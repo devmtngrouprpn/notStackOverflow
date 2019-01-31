c966a29111065489358626c1018bfbb0cd78dabb
McCoy Nelson <mccoytnelson@gmail.com>
2019-01-31 14:32:49 -0700

    merging sql

--- src/components/Tags/Tags.jsx
+++ src/components/Tags/Tags.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Layout from '../Layout/Layout1.jsx';
 import styled from 'styled-components'
 import { TinyTag } from '../../utilites/index.js'
+import { P } from '../../utilites/index'
+import axios from 'axios'
 
 export default function Tags(props) {
     return (
@@ -9,10 +11,10 @@ export default function Tags(props) {
             <Layout>
                 <Content>
                     <Title>Tags</Title>
-                    <Flex>
-                        <TinyTag subject='javascript' />
-                        <TinyTag subject='javascript' />
-                    </Flex>
+                    <Desc>A tag is a keyword or label that categorizes your question with other, similar questions. Using the right tags makes it easier for others to find and answer your question.</Desc>
+                    <Grid>
+                        {<MapReturn>Oh its you</MapReturn>}
+                    </Grid>
                 </Content>
             </Layout>
         </>
@@ -21,9 +23,20 @@ export default function Tags(props) {
 const Content = styled.div`
 padding: 24px;
 `
-const Title = styled.div`
+const Desc = styled(P)`
+font-size:14px;
+`
+const MapReturn = styled.div`
+
+`
+const Title = styled(P)`
 font-size:25px;
 `
-const Flex = styled.div`
-display:flex;
-`
+const Grid = styled.div`
+position: relative;
+  height: 100%;
+  width:100%;
+  display: grid;
+    grid-gap: 8px;
+    grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+`;
