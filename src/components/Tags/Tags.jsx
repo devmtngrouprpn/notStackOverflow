ad0f4c1ccab8f1981d217c3fd62703003af7c0f1
McCoy Nelson <mccoytnelson@gmail.com>
2019-01-31 20:53:44 -0700

    merge me so payton

--- src/components/Tags/Tags.jsx
+++ src/components/Tags/Tags.jsx
@@ -9,45 +9,72 @@ export default class Tags extends Component {
     constructor() {
         super();
         this.state = {
-            allTags: []
+            data: { allTags: [], day: [], week: [] }
         };
-        componentDidMount = async () => {
-            let res = await axios.get('/api/tags/alltinytags');
-            this.setState({ allTags: res.data })
-            console.log(res.data.allTags)
-        }
-        getTags = async () => {
-
-        }
-        render() {
-            return (
-                <>
-                    <Layout>
-                        <Content>
-                            <Title>Tags</Title>
-                            <Desc>A tag is a keyword or label that categorizes your question with other, similar questions. Using the right tags makes it easier for others to find and answer your question.</Desc>
-                            <Grid>
-                                {
-                                    this.state.allTags.map((e) => {
-                                        return <MapReturn>
-                                            <Top>
-                                                <TinyTag subject={`${e.name}`} />
-                                                <QuestionsApartOf>
-                                                    x {e.qtagname}
-                                                </QuestionsApartOf>
-                                            </Top>
-                                        </MapReturn>
-                                    })
-                                }
-                            </Grid>
-                        </Content>
-                    </Layout>
-                </>
-            )
-        }
     };
+    componentDidMount = async () => {
+        let res = await axios.get('/api/tags/alltinytags');
+        this.setState({ data: res.data })
+        console.log(this.state)
+    }
+    getTags = async () => {
+
+    }
+    render() {
+        return (
+            <>
+                <Layout>
+                    <Content>
+                        <Title>Tags</Title>
+                        <Desc>A tag is a keyword or label that categorizes your question with other, similar questions. Using the right tags makes it easier for others to find and answer your question.</Desc>
+                        <Grid>
+                            {
+                                this.state.data.allTags.map((e) => {
+                                    return <MapReturn>
+                                        <Top>
+                                            <TinyTag subject={`${e.name}`} />
+                                            <QuestionsApartOf>
+                                                x {e.questions_with_tag}
+                                            </QuestionsApartOf>
+                                        </Top>
+                                        <TagDescription>
+                                            {e.description}
+                                        </TagDescription>
+                                        <Asked>
+                                            <span>{
+                                                this.state.data.day.map((f) => { if (f.name === e.name) { return f.question_tag + ' asked today, ' } })
+                                            }</span>
+                                            <span>{
+                                                this.state.data.week.map((c) => { if (c.name === e.name) { return c.question_tag + ' asked this week' } })
+                                            } </span>
+                                        </Asked>
+                                    </MapReturn>
+                                })
+                            }
+                        </Grid>
+                    </Content>
+                </Layout>
+            </>
+        )
+    }
 }
+const TagDescription = styled(P)`
+    box-sizing: content-box;
+    font-size: 12px;
+    height: 39px;
+    line-height: 14px;
+    overflow: hidden;
+    margin-bottom: 4px;
+    color: #848d95;
+`
+const Asked = styled(P)`
+margin-top: 10px;
+margin-bottom: 10px;
+font-size: 12px;
+`
 const QuestionsApartOf = styled(P)`
+font-size: 11px;
+    color: #6a737c;
   position: relative;
   top: 10px;
 `;
@@ -64,7 +91,11 @@ const Content = styled.div`
 const Desc = styled(P)`
   font-size: 14px;
 `;
-const MapReturn = styled.div``;
+const MapReturn = styled.div`
+  border-bottom: 1px dotted #e4e6e8;
+  overflow: hidden;
+  height 
+`;
 const Title = styled(P)`
   font-size: 25px;
 `;
