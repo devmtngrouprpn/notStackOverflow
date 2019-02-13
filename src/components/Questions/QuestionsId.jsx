a164041d79f5f819dd87f946f7de11ed60c66c6d
Ryan Hutchison <ryanzhutch@gmail.com>
2019-02-13 14:05:39 -0700

    save

--- src/components/Questions/QuestionsId.jsx
+++ src/components/Questions/QuestionsId.jsx
@@ -8,6 +8,7 @@ import UserTag from "../../utilites/UserTag";
 import ArrowColumn from "../../utilites/ArrowColumn";
 import CommentSection from "./CommentSection";
 import ReactHtmlParser from "react-html-parser";
+import Ads from "./../../utilites/RightSideBar";
 import {
   Page,
   Adds,
